37d0ede76f6d7b4a24641c5742fe46c5935dfd3b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-09 22:53:02 -0300

    cambie la version

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='users-model',
-          version='0.0.2',
+          version='0.0.3',
           description='Modelo de usuarios',
           url='https://github.com/pablodanielrey/users-model',
           author='Desarrollo DiTeSi, FCE',
