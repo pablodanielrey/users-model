382f456a857c853d39008a075d8bf9d5e2c9ed2d
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-16 23:15:01 -0300

    incremento la version

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='users-model',
-          version='0.0.11',
+          version='0.0.12',
           description='Modelo de usuarios',
           url='https://github.com/pablodanielrey/users-model',
           author='Desarrollo DiTeSi, FCE',
