dce1fbccc19993bfecb985d0f7f7564011afb7c5
Miguel Angel Macagno <miguel.macagno@gmail.com>
2019-12-19 08:48:50 -0300

    Version actualizada

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='users-model',
-          version='0.0.20',
+          version='0.0.21',
           description='Modelo de usuarios',
           url='https://github.com/pablodanielrey/users-model',
           author='Desarrollo DiTeSi, FCE',
