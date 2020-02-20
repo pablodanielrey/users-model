1086e140196c6f9f05e826471b0e6745d8cf8570
Miguel Angel Macagno <miguel.macagno@gmail.com>
2020-02-20 09:52:30 -0300

    Se agregan metodos de obtencion de titulos

--- src/users/model/UsersModel.py
+++ src/users/model/UsersModel.py
@@ -6,9 +6,9 @@ import logging
 import json
 
 from sqlalchemy import or_, and_
-from sqlalchemy.orm import joinedload, contains_eager
+from sqlalchemy.orm import joinedload, contains_eager, defer
 
-from .entities.User import User, Mail, Phone, IdentityNumber
+from .entities.User import User, Mail, Phone, IdentityNumber, UserDegree, File
 
 class UsersModel:
 
@@ -66,4 +66,42 @@ class UsersModel:
         u = q.one_or_none()
         if not u:
             return None
-        return u.id        
+        return u.id
+
+    @classmethod
+    def get_person_degrees(cls, session, uid):
+        """
+            Obtiene los titulos para el usuario solicitado
+        """
+        q = session.query(UserDegree).filter(UserDegree.user_id == uid, UserDegree.deleted == None)
+        d = q.all()
+        if not d:
+            return None
+        return d
+
+    @classmethod
+    def get_person_degree(cls, session, uid,did):
+        """
+            Obtiene el titulo solicitado
+        """
+        q = session.query(UserDegree).filter(UserDegree.user_id == uid, UserDegree.id == did, UserDegree.deleted == None)
+        d = q.one_or_none()
+        if not d:
+            return None
+        return d
+
+    @classmethod
+    def delete_person_degree(cls, session, uid, tid):
+        """
+            Elimina el titulo de la persona
+        """
+        t = session.query(UserDegree).filter(UserDegree.deleted == None, UserDegree.id == tid, UserDegree.user_id == uid).first()
+        if t:
+            t.deleted = datetime.datetime.utcnow()
+            session.add(t)
+            return tid
+        return None
+ 
+    @classmethod
+    def get_file(cls, session, fid):
+        return session.query(File).filter(File.id == fid).options(defer('content')).one_or_none()       
